1a674878db10211139838da68b8d268f5f552bde
github username <mwilliamszoe@gmail.com>
2019-05-12 15:11:25 -0500

    basics exercises

---
+++ basics.rb
@@ -0,0 +1,26 @@
+# 1.
+puts "zoe" + " williams"
+
+# 2
+puts 1234 / 1000
+puts 1234 % 1000 / 100
+puts 1234 % 1000 % 100 / 10
+puts 1234 % 1000 % 100 % 10
+
+# 3/4
+movies = {:mv1 => 1975, :mv2 => 2004, :mv3 => 2013, :mv4 => 2001, :mv5 => 1981}
+puts movies.values
+
+# 5
+puts 5*4*3*2
+puts 6*5*4*3
+puts 7*6*5*4
+puts 8*7*6*5
+
+# 6
+puts 1**2.to_f
+puts 2**2.to_f
+puts 3**2.to_f
+
+# 7
+# There is a missing parenthases or bracket or both
