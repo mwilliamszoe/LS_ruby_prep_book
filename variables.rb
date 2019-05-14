04f1d45ae472e468a25116e447ac8e37bfd502eb
github username <mwilliamszoe@gmail.com>
2019-05-13 19:29:20 -0500

    finished variables

---
+++ variables.rb
@@ -0,0 +1,25 @@
+# # 1
+# puts "Whats your name?"
+# name = gets.chomp
+# puts "wasup " + name + "!"
+
+# 2
+# puts "how old are you?"
+# age = gets.chomp
+# puts "In 10 years you will be #{age.to_i + 10}"
+
+# 3
+# 10.times do |n|
+#   puts name
+# end
+
+# 4
+puts "Whats your first name?"
+first_name = gets.chomp
+puts "Whats your last name?"
+last_name = gets.chomp
+puts "wasup " + first_name + " " + last_name + "!"
+
+# 5
+
+# 6
